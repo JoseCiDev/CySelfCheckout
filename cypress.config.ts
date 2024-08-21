8aac553e018fffe22a74ab8d67e43346ad1dd250
cidev3 <josecidev3@gmail.com>
2024-08-21 12:26:44 -0300

    Finalizado configuracao cucumber no projeto

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,15 +1,36 @@
 import { defineConfig } from "cypress";
+import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
+import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
+import { createEsbuildPlugin } from "@badeball/cypress-cucumber-preprocessor/esbuild";
+
+
 
 export default defineConfig({
-  "projectId": "tx3tt6",
-
+  projectId: "tx3tt6",
   includeShadowDom: true,
   defaultCommandTimeout: 10000,
+
   e2e: {
-    setupNodeEvents(on, config) {
+    async setupNodeEvents(
+      on: Cypress.PluginEvents,
+      config: Cypress.PluginConfigOptions
+    ): Promise<Cypress.PluginConfigOptions> {
+      // Necessário para o preprocessor gerar relatórios JSON após cada execução, entre outras coisas
+      await addCucumberPreprocessorPlugin(on, config);
+
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+
+      // Certifique-se de retornar o objeto de configuração, pois ele pode ter sido modificado pelo plugin.
+      return config;
     },
+
     supportFile: 'cypress/support/e2e.{js,jsx,ts,tsx}',
-    specPattern: 'cypress/**/*.{js,jsx,ts,tsx}',
+    specPattern: 'cypress/**/*.{js,jsx,ts,tsx,feature}',
     redirectionLimit: 5000,
     viewportHeight: 1280,
     viewportWidth: 1024,
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+import * as path from 'path';
+
+module.exports = {
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+          },
+        ],
+      },
+    ],
+  },
+};
