5a67a8b78d763e852f6be58aa90991acbc075f3d
cidev3 <josecidev3@gmail.com>
2024-09-05 11:48:45 -0300

    Alterado arquivo de testes cucumber para visitar supermonitor e retornar bem vindo em varios idiomas

--- cypress/e2e/test.feature
+++ cypress/e2e/test.feature
@@ -1,5 +1,5 @@
 # cypress/e2e/duckduckgo.feature
 Feature: duckduckgo.com
   Scenario: visiting the frontpage
-    When I visit duckduckgo.com
-    Then I should see a search bar
+    When I visit application
+    Then Here I return a welcome message in several languages
--- cypress/e2e/test.ts
+++ cypress/e2e/test.ts
@@ -1,10 +1,16 @@
 // cypress/e2e/duckduckgo.ts
 import { When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
-When("I visit duckduckgo.com", () => {
-  cy.log("https://www.duckduckgo.com");
+When("I visit application", () => {
+  cy.visit("http://sm-hkm.docker.local:8080/");
 });
 
-Then("I should see a search bar", () => {
-  cy.log('Teste')
+Then("Here I return a welcome message in several languages", () => {
+  cy.log('Welcome!');
+  cy.log('¡Bienvenido!');
+  cy.log('Willkommen!');
+  cy.log('Powitanie!');
+  cy.log('Hoş geldiňiz!');
+  cy.log('歡迎！');
+  
 });
